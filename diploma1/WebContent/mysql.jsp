a117e11807986adc85014bb2909bbb3cf915bb10
shakhiza <shakhiza@192.168.1.43>
2017-05-16 00:51:05 +0600

    design content, profile

--- diploma1/WebContent/mysql.jsp
+++ diploma1/WebContent/mysql.jsp
@@ -1,5 +1,5 @@
-<%@page import="main.DBConnection"%>
 <%@page import="java.sql.Connection"%>
+<%@page import="main.DBConnection"%>
 <%
  final Connection con = (new DBConnection()).getConnection();
 %>
--- diploma1/src/main/DBConnection.java
+++ diploma1/src/main/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 		Connection con;
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/funny_kazakh?useUnicode=true&characterEncoding=UTF-8", "root", "");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/funny_kazakh?useUnicode=true&characterEncoding=UTF-8", "root", "root");
 			return con;
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
